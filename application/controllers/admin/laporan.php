ae29c269c6ca83e8a37d2a036c3d1895c0c0dddd
sehia99 <budifitt@gmail.com>
2020-08-16 20:39:16 +0700

    fix

--- application/controllers/admin/laporan.php
+++ application/controllers/admin/laporan.php
@@ -3,6 +3,19 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Laporan extends CI_Controller {
 
+	public function __construct(){
+        parent::__construct();
+        if($this->session->userdata('role_id') !='1'){
+            $this->session->set_flashdata('pesan','<div class="alert alert-danger alert-dismissible fade show" role="alert">
+            <strong>Anda Belum Login Sebagai Admin</strong>
+            <button class="close" type="button" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>');
+        redirect('auth/login');
+        }
+    } 
+
 	public function index()
 	{
 		date_default_timezone_set('Asia/Jakarta');
